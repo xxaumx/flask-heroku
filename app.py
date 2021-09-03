9b0e6422bda222d4f9b6288ebb396cf441ccfe8b
xxaumx <89238427+xxaumx@users.noreply.github.com>
2021-09-03 14:38:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello อนัณญา สุขคง เลขที่ 34 ชั้น ม.4/7"
 
 @app.route('/api', methods=['GET'])
 def get_api():
